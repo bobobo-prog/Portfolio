a3760f1876f7bb730198b161c74d30f146f9f9d8
bobobo-prog <ashwin.mp@somaiya.edu>
2020-09-22 00:40:39 +0530

    Graph update

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
+from os import name
 from django.urls import path
 from .views import HomePage,About
+from . import views
 
 urlpatterns = [
     path('',HomePage.as_view(),name = 'home'),
-    path('about/',About.as_view(),name = 'about')
+    path('about/',About.as_view(),name = 'about'),
+    path('graph/',views.Graph,name = 'graph')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,25 +2,12 @@ from django.http import request
 from django.shortcuts import render 
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from plotly.offline import plot
-from plotly.graph_objects import Scatter
-
+from graphos.sources.simple import SimpleDataSource
+from graphos.renderers.gchart import LineChart
 
 
 def Graph(request):
-    x_data = [0,1,2,3,4]
-    y_data = [ x**2 for x in x_data ]
-
-    plot_div = plot([Scatter(
-        x = x_data,y = y_data,mode = 'lines',name = 'test',opacity = '0.8',marker_color = 'green'
-    )],output_type='div')
-
-    return render(request,"Graph.html",context={'plot_div':plot_div})
-
-
-
-    
-
+       return render(request, 'pages/Graph.html')
 
 
 class HomePage(TemplateView):
