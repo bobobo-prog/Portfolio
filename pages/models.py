82bbe8380e157f5e04dbee767523b9604cdf14d9
bobobo-prog <ashwin.mp@somaiya.edu>
2020-09-23 18:32:27 +0530

    DB added for graph gen

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
+from datetime import datetime, timezone
 from django.db import models
 
-# Create your models here.
+class Count(models.Model):
+    id = models.AutoField(primary_key=True)
+    host = models.TextField()
+    vis_month = models.IntegerField(default=0)
+
+    def __str__(self):
+        return(self.host[:50] + " " + str(self.vis_month))
+
+
+
--- pages/views.py
+++ pages/views.py
@@ -3,18 +3,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 import requests
-from requests import status_codes
 import json
-
+from .models import Count
+from datetime import date, datetime
 
 
 def HomePage(request):
-    num_vis = request.session.get('num_visits',0)
-    request.session['num_visits'] = num_vis + 1
-    counts = [ i for i in range(0,num_vis)]
-    counts = json.dumps(counts)
-    context = {'hits' : num_vis,'values':counts}
-    
+    user = request.get_host()
+    mon = datetime.now().month
+    c = Count()
+    c.host = user
+    c.vis_month = mon
+    c.save()
+
+    n = Count.objects.all().count()
+    num_users = [ i for i in range(0,n)]
+    num_users = json.dumps(num_users)
+    context = {'count':num_users}
     return render(request, 'pages/home.html',context)
 
     
@@ -31,4 +36,9 @@ class About(TemplateView):
 
 
     
-    
+
+#     num_vis = request.session.get('num_visits',0)
+#     request.session['num_visits'] = num_vis + 1
+#     counts = [ i for i in range(0,num_vis)]
+#     counts = json.dumps(counts)
+#     context = {'hits' : num_vis,'values':counts}    
