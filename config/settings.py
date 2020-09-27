3faf9b5cfcea1255e9c955c7315877ab8a9e51fe
bobobo-prog <ashwin.mp@somaiya.edu>
2020-09-27 15:25:14 +0530

    PG stuff

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default' : dj_database_url.config()
+    'default': dj_database_url.config()
 }
 
 
