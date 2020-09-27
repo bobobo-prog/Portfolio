65330f7e6b84305365deb945773dfb6803a7a130
bobobo-prog <ashwin.mp@somaiya.edu>
2020-09-27 15:03:03 +0530

    PG stuff

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,12 +79,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'firstgre',
-        'USER':'postgres',
-        'PASSWORD':'ashwin21f2.',
-        'HOST':'127.0.0.1',
-        'PORT':5432,
+        dj_database_url.config()
     }
 }
 
@@ -127,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+
