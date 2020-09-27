cef3be9ae8f676d48d4f5ffe8d9609d2dacb48a4
bobobo-prog <ashwin.mp@somaiya.edu>
2020-09-27 15:07:05 +0530

    PG stuff

--- config/settings.py
+++ config/settings.py
@@ -77,11 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        dj_database_url.config()
-    }
-}
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
