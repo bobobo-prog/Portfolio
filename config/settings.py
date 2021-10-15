ce0152443118479692d86b07d9f76c6321ac89c8
bobobo-prog <ashwin.mp@somaiya.edu>
2021-10-15 20:29:19 +0530

    settings file change for static dirs

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
+STATIC_URL = './pages/static/'
 
 
 STATICFILES_DIRS = [
